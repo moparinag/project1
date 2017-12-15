e009c006d352b7ee70e2f13f830a8c5de084548c
TYSS <TYSS@TYSS-4F-085>
2017-12-15 10:20:41 +0530

    sdfdfgdfg

---
+++ src/CountmenusISTQB.java
@@ -0,0 +1,22 @@
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class CountmenusISTQB 
+{
+static
+{
+	System.setProperty("webdriver.chrome.driver", "./Drivers/chromedriver.exe");
+}
+	public static void main(String[] args) throws InterruptedException 
+	{
+		WebDriver driver=new ChromeDriver();
+		Thread.sleep(20000);
+		driver.get("http://testyantra.com");
+		List<WebElement> elements = driver.findElements(By.xpath("//a[@class='dropdown-toggle']"));
+	    System.out.println(elements.size());
+	}
+}
---
+++ src/countmenus.java
@@ -0,0 +1,32 @@
+
+public class countmenus {
+
+	public static void main(String[] args) 
+	{
+		String s="Qspiders to testyantra";
+		char[] ch=s.toCharArray();
+		s="";
+		for(int i=0;i<ch.length;i++)
+		{
+			int k=i;
+			while(ch[i]!=' ' && i<ch.length-1)
+			{
+				i++;
+			}
+			int j=i;
+			while(j>=k)
+			{
+				s=s+ch[j];
+				j--;
+			}
+			
+			if(i<ch.length-1)
+			{
+				s=s+ch[i];
+			}
+		}
+		System.out.println(s);
+
+	}
+
+}
